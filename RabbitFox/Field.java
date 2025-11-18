fbb846265a6e1372f7010674f9a6a8a5b26f29e5
arkananta47 <kidzaqilx7@gmail.com>
2025-11-18 11:51:15 +0700

    Add files via upload

---
+++ RabbitFox/Field.java
@@ -0,0 +1,83 @@
+
+/**
+ * Write a description of class field here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.List;
+import java.util.ArrayList;
+public class Field
+{
+    private Object[][] field;
+    
+    public Field(int depth, int width){
+        field = new Object[depth][width];
+    }
+    
+    public int getDepth() {
+        return field.length;
+    }
+    
+    public int getWidth() {
+        return field[0].length;
+    }
+    
+    public void clear(Location location) {
+        field[location.getRow()][location.getCol()] = null;
+    }
+    
+    public void place(Object object, Location location) {
+        field[location.getRow()][location.getCol()] = object;
+    }
+    
+    public Object getObjectAt(Location location){
+        return field[location.getRow()][location.getCol()];
+    }
+    
+    public Location freeAdjacentLocation(Location location) {
+        List<Location> free = new ArrayList<>();
+        List<Location> adjacent = adjacentLocations(location);
+        for (Location loc : adjacent) {
+            if(getObjectAt(loc) == null) {
+                free.add(loc);
+            }
+        }
+        
+        return free.isEmpty() ? null : free.get(0);
+    }
+    
+    public List<Location> adjacentLocations(Location location) {
+        List<Location> locations = new ArrayList<>();
+    
+        int row = location.getRow();
+        int col = location.getCol();
+    
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int dc = -1; dc <= 1; dc++) {
+                if (dr == 0 && dc == 0) continue; 
+                
+                int newRow = row + dr;
+                int newCol = col + dc;
+    
+                if (newRow >= 0 && newRow < field.length &&
+                    newCol >= 0 && newCol < field[0].length) {
+                    locations.add(new Location(newRow, newCol));
+                }
+            }
+        } return locations;
+    }
+    
+    public void print2d() {
+        for (int r = 0; r < field.length; r++) {
+            for (int c = 0; c < field[0].length; c++) {
+                Object obj = field[r][c];
+                if (obj instanceof Fox) System.out.print("F ");
+                else if (obj instanceof Rabbit) System.out.print("R ");
+                else System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ RabbitFox/Simulator.java
@@ -0,0 +1,62 @@
+
+/**
+ * Write a description of class simulator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+public class Simulator
+{
+    private Field field;
+    private List<Animal> animals;
+    
+    public Simulator(int depth, int width){
+        field = new Field(depth, width);
+        animals = new ArrayList<>();
+        populate();
+    }
+    
+    public void simulate(int steps) {
+        for (int step = 1; step <= steps; step++){
+            List<Animal> newAnimals = new ArrayList<>();
+            System.out.println("Step " + step + " Foxes: " + count(Fox.class) + " Rabbits: " + count(Rabbit.class));
+            field.print2d();
+            
+            for (Animal animal: new ArrayList<>(animals)) {
+                animal.act(newAnimals);
+                if (!animal.isAlive()) animals.remove(animal);
+            }
+            animals.addAll(newAnimals);
+        }
+    }
+    
+    private void populate(){
+        for (int row = 0; row < field.getDepth(); row++) {
+            for (int col = 0; col < field.getWidth(); col++) {
+                double rand = Math.random();
+                Location loc = new Location(row, col);
+    
+                if (rand < 0.08) {          
+                    Fox fox = new Fox(field, loc);
+                    animals.add(fox);
+                } else if (rand < 0.18) {   
+                    Rabbit rabbit = new Rabbit(field, loc);
+                    animals.add(rabbit);
+                } else {
+                    field.clear(loc);
+                }
+            }
+        }
+    }
+    
+    private int count(Class cls) {
+        int c = 0;
+        for (Animal a : animals)
+            if (cls.isInstance(a)) c++;
+        return c;
+    }
+
+}
